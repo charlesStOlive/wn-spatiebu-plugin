fead065f3bb79a2c9319a56382c84e3c6518e02a
Charles <charles@notilac.fr>
2023-07-12 15:56:07 +0200

    first version

---
+++ Plugin.php
@@ -0,0 +1,78 @@
+<?php namespace Waka\Wbu;
+
+use Backend;
+use Backend\Models\UserRole;
+use System\Classes\PluginBase;
+use Illuminate\Foundation\AliasLoader;
+use App;
+
+/**
+ * wbu Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    public $require = [
+        'Waka.Wutils',
+    ];
+    /**
+     * Returns information about this plugin.
+     */
+    public function pluginDetails(): array
+    {
+        return [
+            'name'        => 'waka.wbu::lang.plugin.name',
+            'description' => 'waka.wbu::lang.plugin.description',
+            'author'      => 'waka',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     */
+    public function register(): void
+    {
+        App::register(\Spatie\Backup\BackupServiceProvider::class);
+        $registeredAppPathConfig = require __DIR__ . '/config/backup.php';
+        $registeredWcliAppPath = plugins_path('wcli/wconfig/config/backup.php');
+        if (file_exists($registeredWcliAppPath)) {
+            $registeredWcliAppPathConfig = require $registeredWcliAppPath;
+            \Config::set('backup', $registeredWcliAppPathConfig);
+        } else {
+            \Config::set('backup', $registeredAppPathConfig);
+        }
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     */
+    public function boot(): void
+    {
+
+    }
+
+    /**
+     * Registers any frontend components implemented in this plugin.
+     */
+    public function registerComponents(): array
+    {
+        return []; // Remove this line to activate
+    }
+
+    /**
+     * Registers any backend permissions used by this plugin.
+     */
+    public function registerPermissions(): array
+    {
+        return []; // Remove this line to activate
+    }
+
+    /**
+     * Registers backend navigation items for this plugin.
+     */
+    public function registerNavigation(): array
+    {
+        return []; // Remove this line to activate
+    }
+}
---
+++ lang/fr/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'wbu - backup',
+        'description' => 'Système de backup',
+    ],
+    'permissions' => [
+        'some_permission' => 'Some permission',
+    ],
+];
